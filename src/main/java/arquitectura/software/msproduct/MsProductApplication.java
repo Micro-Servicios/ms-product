d9ed98ddafbd491bed319a9502f5b7a62109d99b
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2021-11-04 09:55:09 -0400

    se implemento el dar de baja al implementar

--- src/main/java/arquitectura/software/msproduct/MsProductApplication.java
+++ src/main/java/arquitectura/software/msproduct/MsProductApplication.java
@@ -2,10 +2,12 @@ package arquitectura.software.msproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
+@EnableDiscoveryClient // para configurar de control
 public class MsProductApplication {
 
 	public static void main(String[] args) {
