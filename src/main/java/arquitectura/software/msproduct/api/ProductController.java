7ca19da2d472e8d8713086f974e8c081125dc499
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2021-11-21 16:51:45 -0400

    se añadio los logs a los endpoint

--- src/main/java/arquitectura/software/msproduct/api/ProductController.java
+++ src/main/java/arquitectura/software/msproduct/api/ProductController.java
@@ -2,6 +2,8 @@ package arquitectura.software.msproduct.api;
 
 import arquitectura.software.msproduct.entity.Product;
 import arquitectura.software.msproduct.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.Optional;
 @RequestMapping("/v1/api/product")
 public class ProductController {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
 
     @Autowired
     private ProductRepository productRepository;
@@ -22,17 +25,22 @@ public class ProductController {
     @RequestMapping(path = "/save", method = RequestMethod.POST)
     public Product saveCustomer(@RequestBody Product product){
         System.out.println("REGISTRAR EL PRODUCTO desde el puerto: "+serverPort);
+        LOGGER.info("SE REGISTRO EL PRODUCTO");
         return productRepository.save(product);
     }
 
     @RequestMapping(path = "/update", method = RequestMethod.PUT)
     public Product updateCustomer(@RequestBody Product product){
         System.out.println("ACTULIZACION DE EL PRODUCTO desde el puerto: "+serverPort);
+
+        LOGGER.info("SE ACTULIZO EL PRODUCTO");
         return productRepository.save(product);
     }
 
     @RequestMapping(path = "/all", method = RequestMethod.GET)
     public List<Product> getAllCustomer(){
+
+        LOGGER.info("SE ENCONTRO LOS PRODUCTOS");
         return productRepository.findAll();
     }
 
@@ -40,8 +48,11 @@ public class ProductController {
     public Boolean getValueProductId(@RequestParam Integer productId){
         Optional<Product> productOptional =productRepository.findById(productId);
         if(productOptional.isPresent()){
+
+            LOGGER.info("SE ENCONTRO EL PRODUCTO");
             return true;
         }else{
+            LOGGER.error("NO EXISTE EL PRODUCTO");
             return false;
         }
     }
